09cd91d610b083f0364a280bdd6304db17f7ea57
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2026-05-09 17:36:05 +0530

    Learn basics of class and objects

--- PythonForDatascience/ClassBasics/basics.py
+++ PythonForDatascience/ClassBasics/basics.py
@@ -1,11 +1,12 @@
 class Circle:
-    def __init__(self, radius, color=None):
-        self.radius = radius
-        self.color = color
+    def __init__(self, __radius, __color=None):
+        self.radius = __radius
+        self.color = __color
 
     def area(self):
         return 3.14 * self.radius ** 2
 
 color = 'blue'
 smallCir = Circle(10, color)
+smallCir.radius = 15    # Access and modify radius attribute
 print(f"Area of {color} circle: {smallCir.area()}")
