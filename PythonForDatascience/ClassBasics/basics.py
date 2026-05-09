4154ad560a24bdc9cc4f08ded8caed5072979651
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2026-05-09 17:48:30 +0530

    Learn how to access private members and modify

--- PythonForDatascience/ClassBasics/basics.py
+++ PythonForDatascience/ClassBasics/basics.py
@@ -1,12 +1,16 @@
 class Circle:
     def __init__(self, __radius, __color=None):
-        self.radius = __radius
-        self.color = __color
+        self.__radius = __radius
+        self.__color = __color
 
     def area(self):
-        return 3.14 * self.radius ** 2
+        return 3.14 * self.__radius ** 2
 
 color = 'blue'
 smallCir = Circle(10, color)
-smallCir.radius = 15    # Access and modify radius attribute
+smallCir.__setattr__('__radius', 15)    # Access and modify radius attribute
 print(f"Area of {color} circle: {smallCir.area()}")
+
+print(smallCir.__getattribute__('__radius'))
+print(dir(smallCir))
+print(smallCir.__dict__)
