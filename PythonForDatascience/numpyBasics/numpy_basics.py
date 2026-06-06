fa4d141f36f700f0c0edfd19fe3c6b8c035a80f7
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2026-06-06 18:38:58 +0530

    Add numpy_basics.py for creating and inspecting a 1D NumPy array

---
+++ PythonForDatascience/numpyBasics/numpy_basics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a 1D array
+arr1d = np.array([1, 2, 3, 4, 5])
+print("1D Array:")
+print(arr1d)
+print(type(arr1d))
+print(arr1d.shape) # Print the shape of the array (number of elements in each dimension)
+print(arr1d.dtype) # Print the data type of the array elements
+print(arr1d.ndim)  # Print the number of dimensions of the array
+print(arr1d.size)  # Print the total number of elements in the array
