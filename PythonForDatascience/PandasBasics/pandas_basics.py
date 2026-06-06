034dc79a8f2f0912bfe011ff6bf1b0450932ba22
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2026-06-06 17:39:06 +0530

    How to extract data based on a condition

--- PythonForDatascience/PandasBasics/pandas_basics.py
+++ PythonForDatascience/PandasBasics/pandas_basics.py
@@ -12,3 +12,7 @@ print(song_details.iloc[0,0])
 """Method to find unique values in a column"""
 unique_artists = df['artist'].unique()
 print(unique_artists)
+
+"""How to extract data based on a condition"""
+songs_after_2000 = df[df['year'] > 2000]
+print(songs_after_2000)
