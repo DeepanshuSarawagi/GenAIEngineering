63dd602a5a8d806e8c1c271f9bdb1f9f8a43bb9f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2026-06-06 12:11:13 +0530

    Add pandas example for reading and analyzing CSV data

---
+++ PythonForDatascience/PandasBasics/pandas_basics.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('../ReadWriteFiles/songs_sample_generated.csv')
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.columns)
+song_title = df['title']
+print(song_title)
