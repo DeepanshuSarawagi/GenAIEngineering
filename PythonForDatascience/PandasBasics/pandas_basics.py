2be414eca7a7aa5ba3c7a0a9d3075032a6e42dd6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2026-06-06 17:36:30 +0530

    How to find unique elements in DF using pandas_basics.py

--- PythonForDatascience/PandasBasics/pandas_basics.py
+++ PythonForDatascience/PandasBasics/pandas_basics.py
@@ -7,3 +7,8 @@ print(df.describe())
 print(df.columns)
 song_details = df[['title', "artist", "year"]]
 print(song_details)
+print(song_details.iloc[0,0])
+
+"""Method to find unique values in a column"""
+unique_artists = df['artist'].unique()
+print(unique_artists)
